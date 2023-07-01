9a00e009d37807175ebbf2e592ca19bdb3353344
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-07-02 00:39:15 +0200

    Implement return address spoofing using the rbx register to pass the method pointer

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,24 +1,27 @@
 #include "RetAddrSpoofer.hpp"
 
-#include <cstdio>
 #include <cassert>
+#include <cstdio>
 #include <cstring>
 
-void __attribute((naked)) ReturnGadget() {
+void __attribute((naked)) ReturnGadget()
+{
 	__asm volatile("leave; ret;");
 }
 void* RetAddrSpoofer::leaveRet = reinterpret_cast<void*>(ReturnGadget);
 
 const int argumentLength = 1000 * 1000 * 8; // approximate the maximum that the stack can handle
 
-size_t TestFunction(const char str[argumentLength]) {
+size_t TestFunction(const char str[argumentLength])
+{
 	assert(__builtin_extract_return_addr(__builtin_return_address(0)) == RetAddrSpoofer::leaveRet);
 	return strlen(str);
 }
 
-int main() {
+int main()
+{
 	char str[argumentLength + 1];
-	for (char& i: str)
+	for (char& i : str)
 		i = 'A';
 	str[argumentLength] = '\0';
 
