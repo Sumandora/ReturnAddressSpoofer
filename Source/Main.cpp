ed9f63911669bcc6dd65e9851e156e7a6959ae42
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-07-14 21:41:51 +0200

    Implement an approach which uses stack args

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,28 +1,32 @@
 #include "RetAddrSpoofer.hpp"
 
-#include <cstdio>
 #include <cassert>
+#include <cstdint>
+#include <cstdio>
 #include <cstring>
 
-void __attribute((naked)) ReturnGadget() {
-	__asm volatile("leave; ret;");
+void __attribute((naked)) ReturnGadget()
+{
+	__asm volatile("ret;");
 }
 void* RetAddrSpoofer::leaveRet = reinterpret_cast<void*>(ReturnGadget);
 
 const int argumentLength = 1000 * 1000 * 8; // approximate the maximum that the stack can handle
 
-size_t TestFunction(const char str[argumentLength]) {
+size_t TestFunction(const char str[argumentLength])
+{
 	assert(__builtin_extract_return_addr(__builtin_return_address(0)) == RetAddrSpoofer::leaveRet);
 	return strlen(str);
 }
 
-int main() {
+int main()
+{
 	char str[argumentLength + 1];
-	for (char& i: str)
+	for (char& i : str)
 		i = 'A';
 	str[argumentLength] = '\0';
 
-	int length = RetAddrSpoofer::Invoke<int, const char[]>(reinterpret_cast<void*>(TestFunction), str);
+	int length = RetAddrSpoofer::Invoke<int>(reinterpret_cast<std::uintptr_t>(str), 0, 0, 0, 0, 0, reinterpret_cast<void*>(TestFunction));
 
 	assert(length == argumentLength);
 
